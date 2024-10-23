51afccceb1a15b74e1e8406a9361b54c011bc25f
harusaku <harusaku0412@yahoo.co.jp>
2024-10-23 16:04:36 -0400

    implement command

--- src/switch-kana-punctuation.tsx
+++ src/switch-kana-punctuation.tsx
@@ -1,17 +1,38 @@
-import { Action, ActionPanel, List } from "@raycast/api";
+import { Action, ActionPanel, closeMainWindow, List } from "@raycast/api";
+import { exec } from "child_process";
+
+type PunctuationType = "JP-JP" | "JP-EN" | "EN-JP" | "EN-EN";
+
+type Punctuation = {
+  type: PunctuationType;
+  title: string;
+  index: number;
+};
+
+const items: Punctuation[] = [
+  { type: "JP-JP", title: "。と、", index: 0 },
+  { type: "JP-EN", title: "。と，", index: 1 },
+  { type: "EN-JP", title: "．と、", index: 2 },
+  { type: "EN-EN", title: "．と，", index: 3 },
+];
+
+const switchPunctuation = async (punctuation: Punctuation) => {
+  exec(
+    `defaults write com.apple.inputmethod.Kotoeri JIMPrefPunctuationTypeKey ${punctuation.index} && killall -HUP imklaunchagent`,
+  );
+  await closeMainWindow();
+};
 
 export default function Command() {
-  const items = ["Kana", "Punctuation"];
-
   return (
     <List isLoading={items === undefined}>
-      {items?.map((item, index) => (
+      {items?.map((item) => (
         <List.Item
-          key={index}
-          title={item}
+          key={item.type}
+          title={item.title}
           actions={
             <ActionPanel>
-              <Action title="Select" onAction={() => console.log(`${item} selected`)} />
+              <Action title="Select" onAction={() => switchPunctuation(item)} />
             </ActionPanel>
           }
         />
